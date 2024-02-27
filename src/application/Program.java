5895a5ac092d04a936018598f41f26c970fc06e9
Marcelo Cortez <mhcortez@gmail.com>
2024-02-27 15:00:48 -0300

    implementando o insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -25,11 +26,16 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n===TESTE32: seller findByAll =======");		
+		System.out.println("\n===TESTE 3: seller findByAll =======");		
 		list = sellerdao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n===TESTE 4: seller insert =======");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerdao.insert(newSeller);
+		System.out.println("inserted ! New id = "+ newSeller.getId()); 
 		
 	}
 
