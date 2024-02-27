bf83c61049541977a2d10601b0995ae5bf6c49b9
Marcelo Cortez <mhcortez@gmail.com>
2024-02-27 15:27:13 -0300

    Update

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,8 +16,8 @@ public class Program {
 		
 		System.out.println("===TESTE 1: seller findById =======");
 		Seller seller = sellerdao.findById(3);
-		
 		System.out.println(seller);
+		
 		
 		System.out.println("\n===TESTE 2: seller findByDepartment =======");
 		Department department = new Department(2,null);
@@ -31,11 +31,24 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+		/*
 		System.out.println("\n===TESTE 4: seller insert =======");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerdao.insert(newSeller);
 		System.out.println("inserted ! New id = "+ newSeller.getId()); 
+		*/
+		
+		System.out.println("\n===TESTE 5: seller Update =======");
+		seller = sellerdao.findById(1);
+		seller.setName("Martha Waine");
+		sellerdao.update(seller);
+		System.out.println("Update finalizado");
+		
+		System.out.println("\n===TESTE 3: seller findByAll =======");		
+		list = sellerdao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
 		
 	}
 
